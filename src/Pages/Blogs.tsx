2ff4cd54e91a5f762fe0446c697ec725ba0a8675
Teja Chennu <50200888+tejachennu@users.noreply.github.com>
2024-07-03 19:02:50 +0530

    Refresh-errors-fix2

--- src/Pages/Blogs.tsx
+++ src/Pages/Blogs.tsx
@@ -10,10 +10,9 @@ import CenterHeader from '../Components/GlobalComponents/CenterHeader';
 const Blogs: React.FC = () => {
   const { category } = useParams();
   const categoryBlogs = newsBlogs.filter((e)=> e.category == category);
-  window.scrollTo(0, 0); 
 
   console.log(category);
-  const { ref,isVisible  } = useIntract();
+  const { ref } = useIntract();
   return (
     <div className='w-full '>
     {category == undefined ? (<BreadCrumbComponent/>):("")}
@@ -21,7 +20,7 @@ const Blogs: React.FC = () => {
     <div className="py-12 bg-white" ref={ref}>
       <div className="px-4 mx-auto max-w-7xl sm:px-6 lg:px-8">
         {category ? (<CenterHeader heading='Category' subHeading={category}/>):(<CenterHeader heading='News and Updates' subHeading='Latest News and Blogs'/>)} 
-        {category ? (<NewsBlogs newsBlogs={categoryBlogs} isVisible={isVisible} />):(<NewsBlogs newsBlogs={newsBlogs} isVisible={isVisible} />)}
+        {category ? (<NewsBlogs newsBlogs={categoryBlogs} isVisible={true} />):(<NewsBlogs newsBlogs={newsBlogs} isVisible={true} />)}
       </div>
     </div>
     </div>
