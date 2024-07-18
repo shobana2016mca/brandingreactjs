f51ec70ac05b5d4118af15bceecbd6956737cf27
Teja Chennu <50200888+tejachennu@users.noreply.github.com>
2024-07-18 21:05:11 +0530

    banner 3

--- src/Components/HomePage/HeroSection.tsx
+++ src/Components/HomePage/HeroSection.tsx
@@ -5,26 +5,27 @@ import "swiper/css/autoplay";
 import "swiper/css/pagination";
 import { A11y, Autoplay } from "swiper/modules";
 import { FiChevronLeft, FiChevronRight } from "react-icons/fi"; // Importing React Icons
+import { Swiper as SwiperInstance } from 'swiper';
 
 const HeroSection: React.FC = () => {
-  const swiperRef = React.useRef(null);
+  const swiperRef = React.useRef<SwiperInstance | null>(null);
 
   const nextSlide = () => {
-    if (swiperRef.current && swiperRef.current.swiper) {
-      swiperRef.current.swiper.slideNext();
+    if (swiperRef.current) {
+      swiperRef.current.slideNext();
     }
   };
 
   const prevSlide = () => {
-    if (swiperRef.current && swiperRef.current.swiper) {
-      swiperRef.current.swiper.slidePrev();
+    if (swiperRef.current) {
+      swiperRef.current.slidePrev();
     }
   };
 
   return (
     <div className="relative max-w-screen-xl max-sm:h-[40vh] mx-auto">
       <Swiper
-        ref={swiperRef}
+        onSwiper={(swiper) => (swiperRef.current = swiper)}
         modules={[A11y, Autoplay]}
         slidesPerView={1}
         draggable={true}
@@ -43,21 +44,15 @@ const HeroSection: React.FC = () => {
               className="object-cover w-full max-sm:h-[40vh]"
             />
             <div className="absolute inset-0 flex items-center justify-start bg-black bg-opacity-20 ">
-            <section
-          
-          className={`flex flex-col justify-center m-4 lg:ml-36 'animate-slideInDown' : ''}`}
-        >
-          <h1
-            className="mb-4 text-lg max-md:mt-4 md:w-[50%] font-bold text-left text-white transition-opacity duration-1000 transform md:text-5xl lg:text-7xl"
-          >
-            Increase Your Web Traffic with Expert Team
-          </h1>
-          <p className="text-white md:mb-6 max-sm:text-sm md:w-[50%] animate-fade-up">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque
-            nisl eros, pulvinar facilisis justo mollis.
-          </p>
-          
-        </section>
+              <section className="flex flex-col justify-center m-4 lg:ml-36">
+                <h1 className="mb-4 text-lg max-md:mt-4 md:w-[50%] font-bold text-left text-white transition-opacity duration-1000 transform md:text-5xl lg:text-7xl">
+                  Increase Your Web Traffic with Expert Team
+                </h1>
+                <p className="text-white md:mb-6 max-sm:text-sm md:w-[50%] animate-fade-up">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque
+                  nisl eros, pulvinar facilisis justo mollis.
+                </p>
+              </section>
             </div>
           </div>
         </SwiperSlide>
@@ -68,21 +63,15 @@ const HeroSection: React.FC = () => {
               className="object-cover w-full max-sm:h-[40vh]"
             />
             <div className="absolute inset-0 flex items-center justify-start pl-4 bg-black bg-opacity-50">
-            <section
-          
-          className={`flex flex-col justify-center m-4 lg:ml-36 'animate-slideInDown' : ''}`}
-        >
-          <h1
-            className="mb-4 text-lg max-md:mt-4 md:w-[50%] font-bold text-left text-white transition-opacity duration-1000 transform md:text-5xl lg:text-7xl"
-          >
-            Increase Your Web Traffic with Expert Team
-          </h1>
-          <p className="text-white md:mb-6 max-sm:text-sm md:w-[50%] animate-fade-up">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque
-            nisl eros, pulvinar facilisis justo mollis.
-          </p>
-          
-        </section>
+              <section className="flex flex-col justify-center m-4 lg:ml-36">
+                <h1 className="mb-4 text-lg max-md:mt-4 md:w-[50%] font-bold text-left text-white transition-opacity duration-1000 transform md:text-5xl lg:text-7xl">
+                  Increase Your Web Traffic with Expert Team
+                </h1>
+                <p className="text-white md:mb-6 max-sm:text-sm md:w-[50%] animate-fade-up">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque
+                  nisl eros, pulvinar facilisis justo mollis.
+                </p>
+              </section>
             </div>
           </div>
         </SwiperSlide>
@@ -93,21 +82,15 @@ const HeroSection: React.FC = () => {
               className="object-cover w-full max-sm:h-[40vh]"
             />
             <div className="absolute inset-0 flex items-center justify-start pl-4 bg-black bg-opacity-50">
-            <section
-          
-          className={`flex flex-col justify-center m-4 lg:ml-36 'animate-slideInDown' : ''}`}
-        >
-          <h1
-            className="mb-4 text-lg max-md:mt-4 md:w-[50%] font-bold text-left text-white transition-opacity duration-1000 transform md:text-5xl lg:text-7xl"
-          >
-            Increase Your Web Traffic with Expert Team
-          </h1>
-          <p className="text-white md:mb-6 max-sm:text-sm md:w-[50%] animate-fade-up">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque
-            nisl eros, pulvinar facilisis justo mollis.
-          </p>
-          
-        </section>
+              <section className="flex flex-col justify-center m-4 lg:ml-36">
+                <h1 className="mb-4 text-lg max-md:mt-4 md:w-[50%] font-bold text-left text-white transition-opacity duration-1000 transform md:text-5xl lg:text-7xl">
+                  Increase Your Web Traffic with Expert Team
+                </h1>
+                <p className="text-white md:mb-6 max-sm:text-sm md:w-[50%] animate-fade-up">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque
+                  nisl eros, pulvinar facilisis justo mollis.
+                </p>
+              </section>
             </div>
           </div>
         </SwiperSlide>
@@ -118,21 +101,15 @@ const HeroSection: React.FC = () => {
               className="object-cover w-full max-sm:h-[40vh]"
             />
             <div className="absolute inset-0 flex items-center justify-start pl-4 bg-black bg-opacity-50">
-            <section
-          
-          className={`flex flex-col justify-center m-4 lg:ml-36 'animate-slideInDown' : ''}`}
-        >
-          <h1
-            className="mb-4 text-lg max-md:mt-4 md:w-[50%] font-bold text-left text-white transition-opacity duration-1000 transform md:text-5xl lg:text-7xl"
-          >
-            Increase Your Web Traffic with Expert Team
-          </h1>
-          <p className="text-white md:mb-6 max-sm:text-sm md:w-[50%] animate-fade-up">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque
-            nisl eros, pulvinar facilisis justo mollis.
-          </p>
-          
-        </section>
+              <section className="flex flex-col justify-center m-4 lg:ml-36">
+                <h1 className="mb-4 text-lg max-md:mt-4 md:w-[50%] font-bold text-left text-white transition-opacity duration-1000 transform md:text-5xl lg:text-7xl">
+                  Increase Your Web Traffic with Expert Team
+                </h1>
+                <p className="text-white md:mb-6 max-sm:text-sm md:w-[50%] animate-fade-up">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque
+                  nisl eros, pulvinar facilisis justo mollis.
+                </p>
+              </section>
             </div>
           </div>
         </SwiperSlide>
@@ -143,21 +120,15 @@ const HeroSection: React.FC = () => {
               className="object-cover w-full max-sm:h-[40vh]"
             />
             <div className="absolute inset-0 flex items-center justify-start pl-4 bg-black bg-opacity-50">
-            <section
-          
-          className={`flex flex-col justify-center m-4 lg:ml-36 'animate-slideInDown' : ''}`}
-        >
-          <h1
-            className="mb-4 text-lg max-md:mt-4 md:w-[50%] font-bold text-left text-white transition-opacity duration-1000 transform md:text-5xl lg:text-7xl"
-          >
-            Increase Your Web Traffic with Expert Team
-          </h1>
-          <p className="text-white md:mb-6 max-sm:text-sm md:w-[50%] animate-fade-up">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque
-            nisl eros, pulvinar facilisis justo mollis.
-          </p>
-          
-        </section>
+              <section className="flex flex-col justify-center m-4 lg:ml-36">
+                <h1 className="mb-4 text-lg max-md:mt-4 md:w-[50%] font-bold text-left text-white transition-opacity duration-1000 transform md:text-5xl lg:text-7xl">
+                  Increase Your Web Traffic with Expert Team
+                </h1>
+                <p className="text-white md:mb-6 max-sm:text-sm md:w-[50%] animate-fade-up">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque
+                  nisl eros, pulvinar facilisis justo mollis.
+                </p>
+              </section>
             </div>
           </div>
         </SwiperSlide>
