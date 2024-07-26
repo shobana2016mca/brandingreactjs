75f10960331638b7a7c2f2db2601f35b60d8caa7
Teja Chennu <50200888+tejachennu@users.noreply.github.com>
2024-07-26 13:22:48 +0530

    push

--- src/Components/HomePage/Compines.tsx
+++ src/Components/HomePage/Compines.tsx
@@ -1,51 +1,68 @@
 import useIntract from "../GlobalComponents/useIntract";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/autoplay";
+import "swiper/css/pagination";
+import { A11y, Autoplay } from "swiper/modules";
 
 interface Companies {
     icon: string;
-  }
+}
 
 const Icons: Companies[] = [
-    {
-      icon: "logoipsome-5.png",
-    }, 
-    {
-      icon: "logo-ipsome4.png",
-    }, 
-    {
-      icon: "logoipsom2.png",
-    }, 
-    {
-      icon: "logo-ipsom1.png",
-    }, 
-    {
-      icon: "logo-ipsom.png",
-    },
-    {
-      icon: "logoipsome-5.png",
-    },
-  ];
+    { icon: "logoipsome-5.png" },
+    { icon: "logo-ipsome4.png" },
+    { icon: "logoipsom2.png" },
+    { icon: "logo-ipsom1.png" },
+    { icon: "logo-ipsom.png" },
+    { icon: "logoipsome-5.png" },
+    { icon: "logoipsome-5.png" },
+    { icon: "logo-ipsome4.png" },
+    { icon: "logoipsom2.png" },
+    { icon: "logo-ipsom1.png" },
+    { icon: "logo-ipsom.png" },
+    { icon: "logoipsome-5.png" },
+];
 
 function Compines() {
+    const { ref, isVisible } = useIntract();
 
-  const { ref, isVisible } = useIntract();
     return (
-        <section className="w-full mx-auto mb-5 bg-white ">
-            <div className="max-w-screen-xl pt-12 mx-auto ">
-        <h1 className="mt-4 text-3xl font-bold text-center text-slate-700">
-          Trusted by Over{" "}
-          <span className="text-3xl text-orange-600 "> 30000+ </span> Companies{" "}
-        </h1>
-        <div ref={ref}  className="grid grid-cols-1 md:grid-col-4 lg:grid-cols-6">
-          {Icons.map((company, index) => (
-            <>
-              <div key={index} className="flex items-center justify-center p-4">
-                <img src={company.icon} alt="Best Compinies" loading="lazy"  className={`h-auto max-w-full animate-fadeUp ${isVisible ? 'animate-popup' : '  '}`} />
-              </div>
-            </>
-          ))}
-        </div>
-        </div>
-      </section>
+        <section className="w-full mx-auto mb-5 bg-white">
+            <div className="max-w-screen-xl pt-12 mx-auto">
+                <h1 className="mt-4 text-3xl font-bold text-center text-slate-700">
+                    Trusted by Over{" "}
+                    <span className="text-3xl text-orange-600">30000+</span> Companies
+                </h1>
+                <div  className="">
+                    <Swiper
+                        modules={[A11y, Autoplay]}
+                        slidesPerView={6}
+                        draggable={true}
+                        edgeSwipeDetection={true}
+                        grabCursor={true}
+                        loop={true}
+                        autoplay={{
+                            delay: 2000,
+                            disableOnInteraction: false,
+                        }}
+                    >
+                        {Icons.map((company, index) => (
+                            <SwiperSlide key={index}>
+                                <div className="flex items-center justify-center p-4">
+                                    <img
+                                        src={company.icon}
+                                        alt="Best Companies"
+                                        loading="lazy"
+                                        className={`h-auto max-w-full ${isVisible ? 'animate-popup' : ''}`}
+                                    />
+                                </div>
+                            </SwiperSlide>
+                        ))}
+                    </Swiper>
+                </div>
+            </div>
+        </section>
     );
 }
 
